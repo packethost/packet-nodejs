52d9cbb72ee75721046e8c14f2af2cc3990c8504
Enzo Hernan Nicolorich <enzonicolorich@gmail.com>
2015-06-04 01:14:21 -0300

    Add add, remove projects methods

--- examples/simple.js
+++ examples/simple.js
@@ -4,8 +4,16 @@ var Packet = require('../lib/packet'),
 
 var api = new Packet(config.apiKey);
 
-api.projects(function(err, data){
-    for(var i = 0; i < data.length; i++){
+api.getProjects(function(err, data){
+    /*for(var i = 0; i < data.length; i++){
         console.log(data[i].name);
-    }
+    }*/
+});
+
+api.addProject({name:'testing nodejs'}, function(err, data) {
+	console.log(data);
+});
+
+api.deleteProject('82936f16-d78a-4978-a17f-56e745eb468e', function(err, res) {
+	//this works, need to check response from request
 });
